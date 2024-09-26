6c1837e98ccf3827f6ac5b16f72476ac917fbeef
llham sy <50142535+ilhamsyabani@users.noreply.github.com>
2024-09-26 15:02:40 +0700

    fix login

--- resources/js/Components/Card.jsx
+++ resources/js/Components/Card.jsx
@@ -1,7 +1,13 @@
 export default function Card({ children }) {
     return (
-        <div className="border-4 border-gray-600 shadow-sm rounded-sm py-16 px-6 w-full xl:w-[24%]">
-            <h3 className="font-bold text-xl mb-2 text-center ">{children}</h3>
+        <div className="border-4 border-gray-600 shadow-md rounded-lg py-6 px-4 w-full xl:w-[24%] transition-transform transform hover:scale-105 hover:shadow-lg">
+        <h3 className="font-bold text-xl mb-2 text-center text-gray-800">{children}</h3>
+        <p className="text-gray-600 text-center">Deskripsi konten di sini</p>
+        <div className="mt-4 flex justify-center">
+            <button className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200">
+                Action Button
+            </button>
         </div>
+    </div>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('Home');
 });
-Route::get('/login', function () {
-    return inertia('Auth/Login');
-});
